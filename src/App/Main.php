5ecd6dc55e5d88d106c8963eef99298f2cfb2294
lutaya1996 <felppt@yandex.ru>
2023-05-06 16:48:43 +0300

    добавили визуализацию к нашим страничкам

--- src/App/Main.php
+++ src/App/Main.php
@@ -8,15 +8,21 @@ class Main
 {
    public static function run()
    {
+      // Создаем объект класса Roter и при этом вызывается
+      // конструктор
       $router = new Router([
          '/' => 'src/views/indexView.php',
          '/cat' => 'src/views/catView.php',
          '/articles' => 'src/views/articlesView.php',
          '/contacts' => 'src/views/contactsView.php',
       ]);
+
+      // Вызываем метод объекта класса Router для переопределния 
+      // страницы ошибки
       $router->setErrorPage('src/views/nakhuiView.php');
       //$router->setErrorPage('src/views/new404.php');
 
+      // Вызываем метод объекта класса Router для отображения страницы
       $router->run();
    }
 }
--- src/Routes/Router.php
+++ src/Routes/Router.php
@@ -4,11 +4,16 @@ namespace tt\Routes;
 
 use tt\Helpers\Printer;
 
+// Инициируем константу, храняющую путь к страинце ошибки
 const DEFAULT_ERROR_PAGE = 'src/views/404.php';
 
 class Router
 {
+   // Храним хеш-таблицу с uri  в виде ключей и роутами в виде значений
    private $routs;
+
+   // Храним путь до фала который будем подключать через
+   // require и тем самым отображать
    private string $errorPagePath;
 
    /**
@@ -16,55 +21,40 @@ class Router
     */
    public function __construct($routes)
    {
+      // в переменную помещаем хештаблицу(ассоциативный массив или мапа)
       $this->routs = $routes;
+      // по умолчанию устанавливаем страницу для ошибки из константы
       $this->errorPagePath = DEFAULT_ERROR_PAGE;
    }
 
+   // метод который позволяет переобпределимть страцицу ошибок
    public function setErrorPage($path)
    {
       $this->errorPagePath = $path;
    }
 
-   //
+   // Run - основной блок логики приложения
    public function run()
    {
-   
+      // получаем uri запроса
       $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
+      // Ищем uri в зарегестрированных роутах
       if (!array_key_exists($uri, $this->routs)) {
+         //  если не нашли роут в наших зарегестрированных роутах, то
+         // отображаем страцу ошибки
          require $this->errorPagePath;
+
+         // После отображения страницы
+         // логика больше не нужна, поэтому закрываем скрипт
          die();
       }
 
+      // Если uri есть в зарегестрированных роутах
+      // то отображем нормальную страницу из зарегестрированных
       require $this->routs[$uri];
    }
 }
 
 
-// class Router
-// {
-//    public $templatePath = "assets" . DIRECTORY_SEPARATOR . "templates";
 
-//    public $routes = [
-//       "/about" => "about.html",
-//       "/p" => "pidora.html",
-//       "404" => "404.html",
-//       "/" => "index.html",
-//       "/index" => "index.html"
-//    ];
-
-//    /**
-//     *  Ebuchiy router
-//     */
-//    public function route(string $uri)
-//    {
-
-//       if (!array_key_exists($uri, $this->routes)) {
-//          echo file_get_contents($this->templatePath . DIRECTORY_SEPARATOR . $this->routes["404"]);
-//          Printer::beautifulP($_SERVER);
-//          die();
-//       }
-
-//       echo file_get_contents($this->templatePath . DIRECTORY_SEPARATOR . $this->routes[$uri]);
-//    }
-// }
