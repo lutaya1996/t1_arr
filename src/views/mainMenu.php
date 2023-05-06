737e17efbbd2d89e91d29c73132551c860f946ec
lutaya1996 <felppt@yandex.ru>
2023-05-06 17:34:20 +0300

    сделали пункты меню активными при переходе по ним

--- src/views/mainMenu.php
+++ src/views/mainMenu.php
@@ -6,18 +6,19 @@ $menu = [
    'Контакты' => '/contacts'
 ];
 
-$counter = 0;
+
+
+
 ?>
 
 <div class="collapse navbar-collapse justify-content-between px-3" id="navbarCollapse">
    <div class="navbar-nav mr-auto py-0">
       <?php foreach ($menu as $key => $value) : ?>
-         <?php if ($counter == 0) : ?>
+         <?php if ($uri == $value) : ?>
             <a href=<?= $value ?> class="nav-item nav-link active"><?= $key ?></a>
          <?php else : ?>
             <a href=<?= $value ?> class="nav-item nav-link"><?= $key ?></a>
          <?php endif; ?>
-         <?php $counter++; ?>
       <?php endforeach; ?>
    </div>
 </div>
